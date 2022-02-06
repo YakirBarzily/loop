25d6808a1d3cc2fa0ab76541da7c42a18356b47d
YakirBarzily <notcooldudet@gmail.com>
2022-02-06 17:05:32 +0200

    loop

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a number");
+            int i;
+            //i = int.Parse(Console.ReadLine());
+            
+            for (i = 0; i < 3; i++)
+            {
+                Console.WriteLine(i * 5);
+            }
+
+            if (i >= 3)
+                Console.WriteLine("false");
+            else
+                Console.WriteLine("true");
+        }
+    }
+}
